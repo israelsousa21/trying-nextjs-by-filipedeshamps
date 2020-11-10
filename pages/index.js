9fd656551961092e6f598a0e012ee86e18926d39
israelsousa21 <me@israelsousa.com.br>
2020-11-09 22:47:10 -0300

    1 - Update contato

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return (
         <div>
             <h1>Home</h1>
-            <a href="/sobre">Sobre</a>
+            <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
         </div>
     )
 }
