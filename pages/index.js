83ce7ab49e202d13a19ecc91baa9cdf62178d93c
israelsousa21 <me@israelsousa.com.br>
2020-11-09 22:52:53 -0300

    1 - Update teste

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home Teste</h1>
             <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
         </div>
     )
